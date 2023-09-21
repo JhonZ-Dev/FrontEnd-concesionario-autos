5e1a462ecc923402ed16c61f2860bf175f216ef3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-20 20:34:43 -0500

    Create nav-data.ts

---
+++ FrontEndConcesionariaAutos/src/app/sidenav/nav-data.ts
@@ -0,0 +1,59 @@
+export const navbarData = [
+    {
+
+        routelink:'usuarios',
+        icon: 'fal fa-users',
+        label:'Registrar Usuario'
+    },
+    {
+
+        routelink:'autos',
+        icon: 'fal fa-cars',
+        label:'Registrar Auto'
+    },
+    {
+
+        routelink:'ventas',
+        icon: 'fal fa-shopping-cart',
+        label:'Añadir Venta'
+    },
+    {
+
+        routelink:'detalle-ventas',
+        icon: 'fal fa-tags',
+        label:'Detalle Ventas'
+    },
+    {
+
+        routelink:'detalles-usuarios',
+        icon: 'fal fa-file',
+        label:'Detalles Usuarios'
+    },
+    {
+
+        routelink:'detalle-autos',
+        icon: 'fal fa-cars',
+        label:'Detalle Autos'
+    },
+    {
+        routelink:'nueva-cita',
+        icon:'fal fa-file',
+        label:'Agregar Cita'
+    },   
+    {
+        routelink:'detalle-cita',
+        icon:'fal fa-file',
+        label:'Detalle Cita'
+    },
+    {
+        routelink:'modelos',
+        icon:'fal fa-cars',
+        label:'Agregar Modelos Autos'
+    },
+   
+    
+
+
+
+
+];
